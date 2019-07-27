0ae66bc5b2902b8320e132e1d3f4fb4de490a267
luvanda <chris.luvanda@hotmail.com>
2019-07-27 16:21:27 +0300

    Newer27

--- test.php
+++ test.php
@@ -1,10 +1,29 @@
 <?php
+include_once 'connection.php';
 if(isset($_POST['bookBtn'])){
-    include_once 'connection.php';
+    
     $id = mysqli_real_escape_string($conn,$_POST['seatID']);
     $state = mysqli_real_escape_string($conn,$_POST['life']);
+    $seat = mysqli_real_escape_string($conn,$_POST['seat']);
     
     $sql = "UPDATE seats SET status='$state' WHERE ID='$id'";
+    $query = mysqli_query($conn,$sql);
+    if($query){
+        $sql = "INSERT INTO bus_layout_record(seatName,bus_stop,busID)
+        VALUES('$seat','$state','1')";
+        $query = mysqli_query($conn,$sql);
+        if($query){
+            header('location:mimi.php?success'); 
+        }
+        else{
+            echo 'query';
+            exit();
+        }
+        
+    }
+}
+else if(isset($_POST['resBtn'])){
+    $sql = "UPDATE seats SET status='empty'";
     $query = mysqli_query($conn,$sql);
     if($query){
         header('location:mimi.php?success'); 
